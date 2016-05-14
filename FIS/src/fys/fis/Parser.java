32d1f063385771e731b78377b2c2b9cc8fffc703
Arno Beekman <arno.beekman@hva.nl>
2016-05-14 22:33:37 +0200

    gson2 object eruit werd niet gebruikt

--- FIS/src/fys/fis/Parser.java
+++ FIS/src/fys/fis/Parser.java
@@ -2,8 +2,6 @@ package fys.fis;
 
 import java.lang.reflect.Type;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.InstanceCreator;
 
 
@@ -12,8 +10,7 @@ public class Parser extends Index implements InstanceCreator<Communication> {
 	private static final long serialVersionUID = 1L;
 	
 	private String function;
-	
-	Gson gson2 = new GsonBuilder().create();
+
 
 	@Override
 	public Communication createInstance(Type type) {
